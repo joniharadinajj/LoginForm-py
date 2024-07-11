95f043ad78b58d975b1d8be343d0d926e462f9a2
JONI <77753643+joniharadinajj@users.noreply.github.com>
2024-07-11 12:05:42 +0200

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,49 @@
+# numri = int(input("Shkruaj nje numer:"))
+# if(numri % 2 == 0):
+#     print(f"Numri: {numri} eshte cift")
+# else:
+#     print(f"Numri {numri} eshte tek")
+
+x = input("Te lutem vendos nje password:")
+
+y = input("Te lutem rishkruaje passwordin:")
+
+if(x == y):
+    print("Miresevini ne Tirana city")
+else:
+    print("Nuk mund te kyqesh ketu.")
+
+# for i in range(0,11):
+#     print("jCoders Academy")
+
+# numri = 0
+# while numri < 5:
+#     print("prsh njerz")
+#     numri = numri + 1
+
+# for i in range (1,11,1):
+#     print(i)
+
+# emri = "BARCELONA"
+# for i in emri:
+#     print(i)
+
+# for letter in "python":
+#     if letter == "y":
+#         continue
+#     elif letter == "o":
+#         break
+#     print("Shkronjat qe i kemi mar:", letter)
+
+
+
+
+# while True:
+#     emri = input("sheno username:")
+#     if emri != "jonharadinaj":
+#         continue
+#     password = injoput("Shkruaje passwordin:")
+#     if password == "jonijoni":
+#         break
+# print("Emri dhe passwordi jane ne rregull")
+# login form me while conntinue dhe break
